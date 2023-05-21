5241573b3b0771283dd0a72a0c76d11f6b99b426
Clothor- <clothor-@student.42.fr>
2023-05-21 03:09:58 +0300

    Init project

---
+++ src/String/s21_string.h
@@ -0,0 +1,6 @@
+#ifndef SRC_S21_STRING_H_
+#define SRC_S21_STRING_H_
+
+#include <stdlib.h>
+
+#endif
---
+++ src/String/s21_strlen.c
@@ -0,0 +1,6 @@
+#include "s21_string.h"
+
+
+int s21_strlen(const char *str) {
+    return 1;
+}
