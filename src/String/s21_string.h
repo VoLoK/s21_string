c78f0e78d4e4333c4f0a0d3683333f2b5e27cf31
Tinskrin <>
2023-05-23 00:36:08 +0300

    Create function s21_strstr

--- src/String/s21_string.h
+++ src/String/s21_string.h
@@ -10,5 +10,6 @@ typedef long unsigned s21_size_t;
 void *s21_memchr(const void *str, int c, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
+char *s21_strstr(const char *haystack, const char *needle);
 
 #endif
