f4854b04df644e246382e4d5e2e3b9c29a127b34
Tinskrin <>
2023-05-22 18:41:56 +0300

    Create func s21_strncpy

--- src/String/s21_string.h
+++ src/String/s21_string.h
@@ -1,7 +1,7 @@
 #ifndef SRC_S21_STRING_H_
 #define SRC_S21_STRING_H_
 
-#include <stdio.h> // for
+#include <stdio.h>  // for
 
 #define s21_NULL ((void *)0)
 
@@ -9,5 +9,6 @@ typedef long unsigned s21_size_t;
 
 void *s21_memchr(const void *str, int c, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 
 #endif
