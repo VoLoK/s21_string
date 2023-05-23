cce816c2c4ac35a541d2805968608d113164844e
vtb4056686 <GLaptev@inno.tech>
2023-05-23 18:18:01 +0300

    modified: s21_memcpy.c

--- src/String/s21_memcpy.c
+++ src/String/s21_memcpy.c
@@ -1,12 +1,12 @@
 #include "s21_string.h"
 
 void *memcpy(void *dest, const void *src, s21_size_t n) {
-    char *source = (char *) src;
-    char *destination = (char *) dest;
-    int i = 0;
-    for (; i < n; i++) {
-        destination[i] = source[i];
-    }
-    destination[i] = '\0';
-    return destination;
+  char *source = (char *)src;
+  char *destination = (char *)dest;
+  int i = 0;
+  for (; i < n; i++) {
+    destination[i] = source[i];
+  }
+  destination[i] = '\0';
+  return destination;
 }
