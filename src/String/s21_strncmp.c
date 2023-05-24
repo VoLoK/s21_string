7a6c3a0a124d88db8ffe385a858c435db98bdc6f
vtb4056686 <GLaptev@inno.tech>
2023-05-24 17:55:12 +0300

    modified: s21_strncmp.c

--- src/String/s21_strncmp.c
+++ src/String/s21_strncmp.c
@@ -2,8 +2,15 @@
 
 int strncmp(const char *str1, const char *str2, size_t n) {
   int result = 0;
-  for (int i = 0; i < n && !result; i++) {
-    if (str1[i] > str2[i]) {
+  int temp = 0;
+  for (int i = 0; i < n && !result && !temp; i++) {
+    if (str1[i] == '\0' && str2[i] == '\0') {
+      temp = 1;
+    } else if (str1[i] == '\0') {
+      result = -1;
+    } else if (str2[i] == '\0') {
+      result = 1;
+    } else if (str1[i] > str2[i]) {
       result = 1;
     } else if (str1[i] < str2[i]) {
       result = -1;
