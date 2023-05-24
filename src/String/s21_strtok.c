d7d01d08965ba2d1c55f481ad6c16156ffb7f5fc
Tinskrin <>
2023-05-25 00:30:47 +0300

    Create test for strstr func

--- src/String/s21_strtok.c
+++ src/String/s21_strtok.c
@@ -1,47 +1,45 @@
 #include "s21_string.h"
 
 char *s21_strtok(char *str, const char *delim) {
-    static char *last;
-    char *ref;
-    int func_work = 1;
-    
-    if ((ref = str) == s21_NULL && (ref = last) == s21_NULL)
-        func_work = 0;
-    
-    for (int i = 0; func_work && *ref && delim[i]; i++) {
-        if (*ref == delim[i]) {
-            ref++;
-            i = -1;
-        }
+  static char *last;
+  char *ref;
+  int func_work = 1;
+
+  if ((ref = str) == s21_NULL && (ref = last) == s21_NULL) func_work = 0;
+
+  for (int i = 0; func_work && *ref && delim[i]; i++) {
+    if (*ref == delim[i]) {
+      ref++;
+      i = -1;
     }
-    
-    if (func_work && *ref == '\0') {
-        ref = s21_NULL;
+  }
+
+  if (func_work && *ref == '\0') {
+    ref = s21_NULL;
+    last = s21_NULL;
+    func_work = 0;
+  }
+  str = ref;
+
+  while (func_work && *ref) {
+    for (s21_size_t i = 0; delim[i]; i++) {
+      if (*ref == delim[i]) {
+        func_work = 0;
+        *ref = '\0';
+        ref++;
+        if (*ref == '\0') ref = s21_NULL;
+        last = ref;
+        break;
+      }
+    }
+
+    if (func_work) {
+      ref++;
+
+      if (*ref == '\0') {
         last = s21_NULL;
-        func_work = 0;
+      }
     }
-    str = ref;
-    
-    while (func_work && *ref) {
-        for (s21_size_t i = 0; delim[i]; i++) {
-            if (*ref == delim[i]) {
-                func_work = 0;
-                *ref = '\0';
-                ref++;
-                if (*ref == '\0')
-                    ref = s21_NULL;
-                last = ref;
-                break;
-            }
-        }
-        
-        if (func_work) {
-            ref++;
-            
-            if (*ref == '\0') {
-                last = s21_NULL;
-            }
-        }
-    }
-    return str;
+  }
+  return str;
 }
--- src/StringUnittest/s21_tests.c
+++ src/StringUnittest/s21_tests.c
@@ -1,34 +1,32 @@
 #include "s21_tests.h"
 
 int main(void) {
-	run_tests();
+  run_tests();
 
-	return 0;
+  return 0;
 }
 
 void run_testcase(Suite *testcase) {
-//	setlocale(LC_ALL, "");
-	static int counter_testcase = 1;
+  //	setlocale(LC_ALL, "");
+  static int counter_testcase = 1;
 
-	if (counter_testcase > 1)
-		putchar('\n');
-	printf("%s%d%s", "CURRENT TEST: ", counter_testcase, "\n");
-	counter_testcase++;
+  if (counter_testcase > 1) putchar('\n');
+  printf("%s%d%s", "CURRENT TEST: ", counter_testcase, "\n");
+  counter_testcase++;
 
-	SRunner *sr = srunner_create(testcase);
+  SRunner *sr = srunner_create(testcase);
 
-	srunner_set_fork_status(sr, CK_NOFORK);
-	srunner_run_all(sr, CK_NORMAL);
+  srunner_set_fork_status(sr, CK_NOFORK);
+  srunner_run_all(sr, CK_NORMAL);
 
-	srunner_free(sr);
+  srunner_free(sr);
 }
 
 void run_tests(void) {
-	Suite *list_cases[] = {
-		suite_strstr(), NULL};
+  Suite *list_cases[] = {suite_strstr(), NULL};
 
-	for (Suite **current_testcase = list_cases; *current_testcase != NULL;
-		 current_testcase++) {
-		run_testcase(*current_testcase);
-	}
+  for (Suite **current_testcase = list_cases; *current_testcase != NULL;
+       current_testcase++) {
+    run_testcase(*current_testcase);
+  }
 }
