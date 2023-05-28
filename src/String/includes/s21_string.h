c8c9f7172e136c7eaa6eb7ff76da5394ad100317
Clothor- <clothor-@student.42.fr>
2023-05-28 20:41:57 +0300

    Added s21_strlen and s21_strncat

--- src/String/includes/s21_string.h
+++ src/String/includes/s21_string.h
@@ -3,15 +3,19 @@
 
 #define s21_NULL ((void *)0)
 
+#define S21_TEXTMAX 2048
+
 typedef long unsigned s21_size_t;
 
 void *s21_memchr(const void *str, int c, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
+char *s21_strncat(char *dest, const char *src, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 char *s21_strstr(const char *haystack, const char *needle);
 char *s21_strtok(char *str, const char *delim);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
+s21_size_t s21_strlen(const char *str);
 
 #endif
--- src/String/s21_memcpy.c
+++ src/String/s21_memcpy.c
@@ -4,8 +4,9 @@ void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
   char *source = (char *)src;
   char *destination = (char *)dest;
   s21_size_t i = 0;
-  for (; i < n; i++) {
+  while (i < n) {
     destination[i] = source[i];
+    i++;
   }
   destination[i] = '\0';
   return destination;
