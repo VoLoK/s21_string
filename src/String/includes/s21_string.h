3ed490adf8630044e23edd41ac9da97745f76f78
Tanya <112762382+kireeva8@users.noreply.github.com>
2023-05-28 15:27:20 +0200

    Slivered (#15)

--- src/String/includes/s21_string.h
+++ src/String/includes/s21_string.h
@@ -10,5 +10,8 @@ void *s21_memset(void *str, int c, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 char *s21_strstr(const char *haystack, const char *needle);
 char *s21_strtok(char *str, const char *delim);
+void *s21_memcpy(void *dest, const void *src, s21_size_t n);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 
 #endif
---
+++ src/String/s21_memcpy.c
@@ -0,0 +1,12 @@
+#include "s21_string.h"
+
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+  char *source = (char *)src;
+  char *destination = (char *)dest;
+  s21_size_t i = 0;
+  for (; i < n; i++) {
+    destination[i] = source[i];
+  }
+  destination[i] = '\0';
+  return destination;
+}
