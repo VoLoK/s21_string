254322ee42d830e29d2678deab908dd759f62cc7
Sullust Gangplank <sullustg@mi-q1.msk.21-school.ru>
2023-05-29 17:28:25 +0300

    3func.2

--- src/String/s21_strchr.c
+++ src/String/s21_strchr.c
@@ -1,10 +1,13 @@
 #include "s21_string.h"
-char* s21_strchr(const char* str, int c) {
-    while (*str != (char)c) {
-        if (!*str) {
-            return NULL;
-        }
+#include <string.h>
+char *s21_strchr(const char* str, int c){
+    char *zavupa[4096];
+    int flag=0;
+    while (*str !='\0')
+    {
+        if (*str == c) *zavupa= (char *) str;
         str++;
+        flag=1;
     }
-    return (char*)str;
-}
+    return (flag!=0) ? *zavupa :NULL;
+}   
--- src/String/s21_strpbrk.c
+++ src/String/s21_strpbrk.c
@@ -1,20 +1,22 @@
 #include "s21_string.h"
+#include <string.h>
 
 char *s21_strpbrk(const char *s1, const char *s2)
 {
+    char *fp[4096];
  int flag=0;
     if((s1 == NULL) || (s2 == NULL))
         flag=1;
     while(*s1 && flag!=1)
     {
-        if(s21_strchr(s2, *s1))
-        {
-            return (char *)s1;
-        }
-        else
+        if(!s21_strchr(s2, *s1))
         {
             s1++;
         }
+        else
+        {
+            flag=1; *fp=(char *)s1;
+        }
     }
-    return NULL;
+    return (flag!=0) ? *fp: NULL;
 }
