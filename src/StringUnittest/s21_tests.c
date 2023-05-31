746ab186af9b3694ae5bf223118d5b5d08a3d25a
Lawnties <99724334+tinskrin@users.noreply.github.com>
2023-05-31 18:56:29 +0300

    Update s21_tests.c

--- src/StringUnittest/s21_tests.c
+++ src/StringUnittest/s21_tests.c
@@ -23,7 +23,7 @@ void run_testcase(Suite *testcase) {
 
 void run_tests(void) {
   Suite *list_cases[] = {suite_strstr(), suite_strncpy(), suite_memset(),
-                         suite_strtok(), suite_memchr(),  NULL};
+                         suite_memchr(),  NULL};
 
   for (Suite **current_testcase = list_cases; *current_testcase != NULL;
        current_testcase++) {
