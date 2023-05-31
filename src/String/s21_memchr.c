6b6f356c73a7188cc8dc143ae90a8efc2bd7e849
Sullust Gangplank <sullustg@mi-q1.msk.21-school.ru>
2023-05-31 19:49:44 +0300

    diverge11

--- src/String/s21_memchr.c
+++ src/String/s21_memchr.c
@@ -1,10 +1,10 @@
 #include "s21_string.h"
 
 void *s21_memchr(const void *str, int c, s21_size_t n) {
-  const char *ptr = (const char *)str;
+  const unsigned char *ptr = (const unsigned char *)str;
+  void *res = s21_NULL;
+  for (s21_size_t i = 0; i < n && !res; i++)
+    if (ptr[i] == c) res = (void *)&ptr[i];
 
-  for (s21_size_t i = 0; i < n; i++)
-    if (ptr[i] == c) return (void *)&ptr[i];
-
-  return s21_NULL;
+  return res;
 }
