2797665f5b56a61d64a293bbb8a62a2204ba51de
Dragonsxs <126290199+Dragonsxs@users.noreply.github.com>
2023-05-31 17:46:54 +0300

    Update s21_strchr.c

--- src/String/s21_strchr.c
+++ src/String/s21_strchr.c
@@ -3,6 +3,7 @@
 char* s21_strchr(const char* str, int c) {
   const char* position = s21_NULL;
   s21_size_t i = 0;
+  
   for (i = 0;; i++) {
     if ((unsigned char)str[i] == c) {
       position = &str[i];
@@ -11,4 +12,5 @@ char* s21_strchr(const char* str, int c) {
     if (str[i] == '\0') break;
   }
   return (char*)position;
+
 }
