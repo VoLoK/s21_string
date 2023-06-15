274eb793e652413832a45190f162ce54659bf6f9
Tinskrin <>
2023-06-15 20:57:34 +0300

    Fixed f soecific func

--- src/String/includes/s21_sprintf.h
+++ src/String/includes/s21_sprintf.h
@@ -8,8 +8,8 @@ char* s21_itoa(int input, char* buff, int num);
 char* s21_convert(char* buff, int size, unsigned int num, int base);
 void d_specific(char*, va_list, char*, unsigned char, int*, char*);
 void c_specific(void);
-void f_specific(va_list list, float value, char* p, char* temp,
-                unsigned char len, int* i, char* str);
+void f_specific(va_list list, char* p, char* temp, unsigned char len, int* i,
+                char* str);
 void s_specific(va_list, char*, unsigned char, int*, char*);
 void u_specific(void);
 void percent_specific(void);
--- src/String/s21_sprintf.c
+++ src/String/s21_sprintf.c
@@ -9,7 +9,6 @@ int s21_sprintf(char* str, const char* format, ...) {
   int size = (int)s21_strlen(format);
   int i = 0;
   unsigned char len = ' ';
-  float value = 0.0;
   while (*format != 0 && i < size) {
     if (*format++ == '%') {
       switch (*format++) {
@@ -20,7 +19,7 @@ int s21_sprintf(char* str, const char* format, ...) {
           d_specific(temp, list, p, len, &i, str);
           break;
         case 'f':
-          f_specific(list, value, p, temp, len, &i, str);
+          f_specific(list, p, temp, len, &i, str);
           break;
         case 's':
           s_specific(list, p, len, &i, str);
@@ -88,9 +87,9 @@ void s_specific(va_list list, char* p, unsigned char len, int* i, char* str) {
 
 void c_specific() {}
 
-void f_specific(va_list list, float value, char* p, char* temp,
-                unsigned char len, int* i, char* str) {
-  value = (float)va_arg(list, double);
+void f_specific(va_list list, char* p, char* temp, unsigned char len, int* i,
+                char* str) {
+  float value = (float)va_arg(list, double);
   p = s21_ftoa(temp, sizeof(temp), value, 0);
   len = (unsigned char)s21_strlen(p);
   s21_strncat(&str[*i], p, s21_strlen(p));
