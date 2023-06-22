374c0361fa990d5b81e47356da4a3c68b8dbbcea
Tanya <112762382+kireeva8@users.noreply.github.com>
2023-06-22 21:08:06 +0300

    Update s21_to_lower.c

--- src/String/s21_to_lower.c
+++ src/String/s21_to_lower.c
@@ -5,7 +5,7 @@ void *s21_to_lower(const char *str) {
     return NULL;
   }
   int len = s21_strlen(str);
-  char *copy_str = (char *)malloc(len + 1 * sizeof(char));
+  char *copy_str = (char *)malloc((len + 1) * sizeof(char));
   for (int i = 0; str[i] != '\0'; i++) { 
     if (str[i] >= 'A' && str[i] <= 'Z') {
       copy_str[i] = str[i] + 32;
