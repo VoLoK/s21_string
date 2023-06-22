4b721d63fd60d4146f81b369128198385bf2c75b
Tanya <112762382+kireeva8@users.noreply.github.com>
2023-06-22 21:08:43 +0300

    Update s21_to_upper.c

--- src/String/s21_to_upper.c
+++ src/String/s21_to_upper.c
@@ -5,7 +5,7 @@ void *s21_to_upper(const char *str) {
     return NULL;
   }
   int len = s21_strlen(str);
-  char *copy_str = (char *)malloc(len * sizeof(char));
+  char *copy_str = (char *)malloc((len + 1) * sizeof(char));
   for (int i = 0; str[i] != '\0'; i++) {
     if (str[i] >= 'a' && str[i] <= 'z') {
       copy_str[i] = str[i] - 32;
