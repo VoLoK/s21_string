b8e5cd0d32eff53b467764d7e4202bc4c127ccd4
Lawnties <99724334+tinskrin@users.noreply.github.com>
2023-06-24 21:53:54 +0300

    create tests for s and d specific of sprintf (#35)

---
+++ src/StringUnittest/s21_sprintf_test.c
@@ -0,0 +1,104 @@
+#include "s21_tests.h"
+
+START_TEST(d_simple_int) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  int s21 = 5;
+  int system = 5;
+    s21_sprintf(s21_buff, "This is %d", s21);
+    sprintf(buff, "This is %d", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(d_negative_int) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  int s21 = -19;
+  int system = -19;
+    s21_sprintf(s21_buff, "This is %d", s21);
+    sprintf(buff, "This is %d", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(d_int_in_middle_of_str) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  int s21 = 1024;
+  int system = 1024;
+    s21_sprintf(s21_buff, "This is %d number", s21);
+    sprintf(buff, "This is %d number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(s_simple_test) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  char* s21 = "Hello str";
+  char* system = "Hello str";
+    s21_sprintf(s21_buff, "This is %s", s21);
+    sprintf(buff, "This is %s", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(s_empty_test) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  char* s21 = "";
+  char* system = "";
+    s21_sprintf(s21_buff, "This is %s", s21);
+    sprintf(buff, "This is %s", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(s_int_str_test) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  char* s21 = "12345";
+  char* system = "12345";
+    s21_sprintf(s21_buff, "This is %s", s21);
+    sprintf(buff, "This is %s", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(s_str_in_middle_of_str) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  char* s21 = "my";
+  char* system = "my";
+  s21_sprintf(s21_buff, "This is %s string", s21);
+  sprintf(buff, "This is %s string", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+
+Suite* suite_sprintf() {
+  Suite* suite = suite_create("sprintf_suite");
+  TCase* tcase_core = tcase_create("sprintf_tc");
+
+  tcase_add_test(tcase_core, d_simple_int);
+  tcase_add_test(tcase_core, d_negative_int);
+  tcase_add_test(tcase_core, d_int_in_middle_of_str);
+  tcase_add_test(tcase_core, s_simple_test);
+  tcase_add_test(tcase_core, s_empty_test);
+  tcase_add_test(tcase_core, s_int_str_test);
+  tcase_add_test(tcase_core, s_str_in_middle_of_str);
+//  tcase_add_test(tcase_core, insert_in_middle);
+
+  suite_add_tcase(suite, tcase_core);
+  return suite;
+}
+
