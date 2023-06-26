9b9184cf2d4851c7891ad3664bd402f35a9cad51
Lawnties <99724334+tinskrin@users.noreply.github.com>
2023-06-26 21:56:21 +0300

    Tests and bag fix (#36)
    Co-authored-by: Lawn Tiesha <lawnties>

--- src/StringUnittest/s21_sprintf_test.c
+++ src/StringUnittest/s21_sprintf_test.c
@@ -5,8 +5,8 @@ START_TEST(d_simple_int) {
   char buff[100] = "";
   int s21 = 5;
   int system = 5;
-    s21_sprintf(s21_buff, "This is %d", s21);
-    sprintf(buff, "This is %d", system);
+  s21_sprintf(s21_buff, "This is %d", s21);
+  sprintf(buff, "This is %d", system);
 
   ck_assert_pstr_eq(s21_buff, buff);
 }
@@ -17,8 +17,8 @@ START_TEST(d_negative_int) {
   char buff[100] = "";
   int s21 = -19;
   int system = -19;
-    s21_sprintf(s21_buff, "This is %d", s21);
-    sprintf(buff, "This is %d", system);
+  s21_sprintf(s21_buff, "This is %d", s21);
+  sprintf(buff, "This is %d", system);
 
   ck_assert_pstr_eq(s21_buff, buff);
 }
@@ -29,8 +29,8 @@ START_TEST(d_int_in_middle_of_str) {
   char buff[100] = "";
   int s21 = 1024;
   int system = 1024;
-    s21_sprintf(s21_buff, "This is %d number", s21);
-    sprintf(buff, "This is %d number", system);
+  s21_sprintf(s21_buff, "This is %d number", s21);
+  sprintf(buff, "This is %d number", system);
 
   ck_assert_pstr_eq(s21_buff, buff);
 }
@@ -41,8 +41,8 @@ START_TEST(s_simple_test) {
   char buff[100] = "";
   char* s21 = "Hello str";
   char* system = "Hello str";
-    s21_sprintf(s21_buff, "This is %s", s21);
-    sprintf(buff, "This is %s", system);
+  s21_sprintf(s21_buff, "This is %s", s21);
+  sprintf(buff, "This is %s", system);
 
   ck_assert_pstr_eq(s21_buff, buff);
 }
@@ -53,8 +53,8 @@ START_TEST(s_empty_test) {
   char buff[100] = "";
   char* s21 = "";
   char* system = "";
-    s21_sprintf(s21_buff, "This is %s", s21);
-    sprintf(buff, "This is %s", system);
+  s21_sprintf(s21_buff, "This is %s", s21);
+  sprintf(buff, "This is %s", system);
 
   ck_assert_pstr_eq(s21_buff, buff);
 }
@@ -65,8 +65,8 @@ START_TEST(s_int_str_test) {
   char buff[100] = "";
   char* s21 = "12345";
   char* system = "12345";
-    s21_sprintf(s21_buff, "This is %s", s21);
-    sprintf(buff, "This is %s", system);
+  s21_sprintf(s21_buff, "This is %s", s21);
+  sprintf(buff, "This is %s", system);
 
   ck_assert_pstr_eq(s21_buff, buff);
 }
@@ -84,6 +84,113 @@ START_TEST(s_str_in_middle_of_str) {
 }
 END_TEST
 
+START_TEST(f_simple) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 4.3;
+  float system = 4.3;
+  s21_sprintf(s21_buff, "This is %f number", s21);
+  sprintf(buff, "This is %f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(f_simple_zero) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 0.34;
+  float system = 0.34;
+  s21_sprintf(s21_buff, "This is %f number", s21);
+  sprintf(buff, "This is %f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(f_big_number) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 9.3425789;
+  float system = 9.3425789;
+  s21_sprintf(s21_buff, "This is %f number", s21);
+  sprintf(buff, "This is %f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(f_big_int_part) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 10.3;
+  float system = 10.3;
+  s21_sprintf(s21_buff, "This is %f number", s21);
+  sprintf(buff, "This is %f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(f_zero) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 0;
+  float system = 0;
+  s21_sprintf(s21_buff, "This is %f number", s21);
+  sprintf(buff, "This is %f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(exactness_simple) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 2.322;
+  float system = 2.322;
+  s21_sprintf(s21_buff, "This is %.2f number", s21);
+  sprintf(buff, "This is %.2f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(exactness_with_simple_rounding) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 2.3228;
+  float system = 2.3228;
+  s21_sprintf(s21_buff, "This is %.3f number", s21);
+  sprintf(buff, "This is %.3f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(exactness_with_hard_rounding) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = 2.3228;
+  float system = 2.3228;
+  s21_sprintf(s21_buff, "This is %.3f number", s21);
+  sprintf(buff, "This is %.3f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
+
+START_TEST(exactness_with_hard_rounding_minus) {
+  char s21_buff[100] = "";
+  char buff[100] = "";
+  float s21 = -2.3228;
+  float system = -2.3228;
+  s21_sprintf(s21_buff, "This is %.3f number", s21);
+  sprintf(buff, "This is %.3f number", system);
+
+  ck_assert_pstr_eq(s21_buff, buff);
+}
+END_TEST
 
 Suite* suite_sprintf() {
   Suite* suite = suite_create("sprintf_suite");
@@ -96,9 +203,16 @@ Suite* suite_sprintf() {
   tcase_add_test(tcase_core, s_empty_test);
   tcase_add_test(tcase_core, s_int_str_test);
   tcase_add_test(tcase_core, s_str_in_middle_of_str);
-//  tcase_add_test(tcase_core, insert_in_middle);
-
+  tcase_add_test(tcase_core, f_simple);
+  tcase_add_test(tcase_core, f_simple_zero);
+  tcase_add_test(tcase_core, f_big_number);
+  tcase_add_test(tcase_core, f_big_int_part);
+  tcase_add_test(tcase_core, f_zero);
+  tcase_add_test(tcase_core, exactness_simple);
+  tcase_add_test(tcase_core, exactness_with_simple_rounding);
+  tcase_add_test(tcase_core, exactness_with_hard_rounding);
+  tcase_add_test(tcase_core, exactness_with_hard_rounding_minus);
+   
   suite_add_tcase(suite, tcase_core);
   return suite;
 }
-
