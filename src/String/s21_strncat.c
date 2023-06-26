c19d63486aa8b749353557193dda733e5eab3368
Clothor- <clothor-@student.21-school.ru>
2023-06-26 18:51:44 +0000

    Update s21_strncat.c

--- src/String/s21_strncat.c
+++ src/String/s21_strncat.c
@@ -4,10 +4,7 @@ char *s21_strncat(char *dest, const char *src, s21_size_t n) {
   char *ref = dest;
 
   while (*ref) ref++;
-    while (n) {
-        *ref++ = *src++;
-        n--;
-    }
+  while (n--) *ref++ = *src++;
   *ref = '\0';
 
   return dest;
