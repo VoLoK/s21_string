15fd18d8d5cf952f7c503554034de6a453d65f89
Clothor- <clothor-@student.21-school.ru>
2023-07-11 21:14:52 +0000

    Update s21_memcpy.c

--- src/String/s21_memcpy.c
+++ src/String/s21_memcpy.c
@@ -4,6 +4,6 @@ void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
   char *source = (char *)src;
   char *destination = (char *)dest;
   while (n-- > 0)
-    *destination++ = *source;
+    *destination++ = *source++;
   return destination;
 }
